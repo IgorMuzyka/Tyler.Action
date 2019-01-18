9a351004f0ee9eefe3bc2b9fbb7dc9229f262f3c
Igor Muzyka <imuz@ciklum.com>
2019-01-18 11:03:16 +0200

    Minor updates

--- Sources/Action/TypePreservingCodingAdapter+Convenience.swift
+++ Sources/Action/TypePreservingCodingAdapter+Convenience.swift
@@ -3,6 +3,7 @@ import TypePreservingCodingAdapter
 
 extension TypePreservingCodingAdapter {
 
+    @discardableResult
     public func register<GenericAction: Action>(action: GenericAction.Type) -> TypePreservingCodingAdapter {
         register(type: GenericAction.self)
         register(alias: GenericAction.alias, for: GenericAction.self)
